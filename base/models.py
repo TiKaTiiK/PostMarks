5ac5d92ed89cf8684911f290a58326653eca286e
Your Name <you@example.com>
2024-07-10 23:51:21 +0400

    added Catalogue: Task 8

--- base/models.py
+++ base/models.py
@@ -10,4 +10,5 @@ class Mark(models.Model):
     description = models.TextField(max_length=300)
     content = models.CharField(max_length=200)
 
-
+    def __str__(self):
+        return f"{self.name} _ {self.author}"
--- base/views.py
+++ base/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Mark
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'base/home.html')
+    marks = Mark.objects.all()
+    context = {"marks": marks}
+    return render(request, 'base/home.html', context)
 
 
 def about(request):
