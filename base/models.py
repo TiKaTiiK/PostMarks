6b8de2732bf48f214a78ecd5d4cbdceaf3c3e2e2
Your Name <you@example.com>
2024-07-12 23:42:29 +0400

    Many2Many: Task 8

--- base/models.py
+++ base/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    birth = models.DateField(null=True)
+    death = models.DateField(null=True)
+
+    def __str__(self):
+        return self.name
 
 # Create your models here.
-
-
 class Mark(models.Model):
-    author = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.SET("Unknown Author"))
     image = models.CharField(max_length=300)
     name = models.CharField(max_length=200)
     description = models.TextField(max_length=300)
@@ -12,3 +18,6 @@ class Mark(models.Model):
 
     def __str__(self):
         return f"{self.name} _ {self.author}"
+
+class User(AbstractUser):
+    marks = models.ManyToManyField(Mark, blank=True, related_name='Mark')
--- base/views.py
+++ base/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Mark
+from .models import Mark, User
 
 # Create your views here.
 
@@ -15,6 +15,8 @@ def about(request):
     return render(request, 'base/about.html')
 
 
-def profile(request):
-    return render(request, 'base/profile.html')
-
+def profile(request, st):
+    user = User.objects.get(id=int(st))
+    marks = user.marks.all()
+    context = {'marks': marks, "user": user}
+    return render(request,'base/profile.html', context)
